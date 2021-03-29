3a607cc563ab087a8a0a79d3e43ee76a5f8a8e89
Grant McDermott <grant.mcdermott@gmail.com>
2021-03-28 18:35:53 -0700

    Basic scaffolding

---
+++ _common.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+	comment = "#>",
+	collapse = TRUE,
+	cache = TRUE,
+	dpi = 300,
+	fig.align = 'center',
+	fig.width = 6,
+	fig.asp = 0.618,  # 1 / phi
+	fig.show = "hold"
+)
